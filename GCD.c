5c3943bb14cf3d349fb2a0a19acd53c14306bfd2
yogitadixit2025-sys <yogita.dixit2025@glbajajgroup.org>
2026-06-11 14:55:46 +0530

    Day3 assignment
    Uploaded through Android phone

---
+++ GCD.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,gcd;
+    printf("Enter two no");
+    scanf("%d%d",&num1,&num2);
+    for(int i=1;i<=num1,&num2;i++)
+    {
+        if(num1%1==0 && num2%i ==0)
+        gcd=i;
+    }
+    printf("GCD of %d and %d is %d",num1);
+    return 0;
+}
